5f8411e7812d34b9bff5e343fd03b8362e9a8543
Зырянов Кирилл <KirillVar1@mail.ru>
2022-12-19 09:43:47 +0500

    models update

--- ripc/models.py
+++ ripc/models.py
@@ -1,50 +1,88 @@
+import uuid
+
 from django.conf import settings
 from django.db import models
 
 
 class Subject(models.Model):
-    sub_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.sub_name
+        return self.name
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=300)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Expert(models.Model):
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    # name = models.CharField(max_length=30)
-    # second_name = models.CharField(max_length=30)
-    # surname = models.CharField(max_length=30)
     mail = models.CharField(max_length=50)
-
-    # def __str__(self):
-    #     return self.second_name + self.second_name + self.surname
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    referee = models.BooleanField()
 
 
 class Admin(models.Model):
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # name = models.CharField(max_length=30)
-    # second_name = models.CharField(max_length=30)
-    # surname = models.CharField(max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return .second_name + self.second_name + self.surname
+
+class RegionRep(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+
+class OrganizationRep(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
 
 class Event(models.Model):
     name = models.CharField(max_length=300)
-    date_begin = models.DateField
-    deadline = models.DateField
+    start_date = models.DateField()
+    end_date = models.DateField()
+    check_times = models.IntegerField()
 
-    # subjects - id's предметов, названия хранить отдельно
     def __str__(self):
-        return self.date_begin
+        return self.name
 
 
-class Organization(models.Model):
-    org_name = models.CharField(max_length=300)
+class Task(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    task = models.UUIDField(default=uuid.uuid4())
 
-    # experts
-    # city
-    def __str__(self):
-        return self.org_name
+
+class TaskExpert(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
+    check_date = models.DateTimeField()
+    check_time = models.IntegerField()
+
+
+class Answer(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    mark = models.CharField(max_length=10)
+    answer = models.UUIDField(default=uuid.uuid4())
+
+
+class ThirdMark(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    check_date = models.DateTimeField()
+    check_time = models.IntegerField()
+
+
+class ThirdMarkExpert(models.Model):
+    third_mark = models.ForeignKey(ThirdMark, on_delete=models.CASCADE)
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
