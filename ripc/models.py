74473b1eef03d6a6c7469f9aec8c4fe513ef9699
Barinov Ilya <stud0000232841@study.utmn.ru>
2023-03-10 01:32:19 +0500

    Объеденил модели базы данных

--- ripc/models.py
+++ ripc/models.py
@@ -1,7 +1,9 @@
 import uuid
 
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Subject(models.Model):
@@ -58,10 +60,21 @@ class Event(models.Model):
         return self.name
 
 
+class Variant(models.Model):
+    file_link = models.TextField()
+    page_len = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+
+class PatternTask(models.Model):
+    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    max_score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+
 class Task(models.Model):
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    task = models.UUIDField(default=uuid.uuid4())
+    variant_id = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    pattern_id = models.ForeignKey(PatternTask, on_delete=models.CASCADE)
+    answer_coord = ArrayField(ArrayField(models.IntegerField()))
+    file_link = models.TextField()
 
 
 class TaskExpert(models.Model):
@@ -71,10 +84,18 @@ class TaskExpert(models.Model):
     check_time = models.IntegerField()
 
 
+class Complect(models.Model):
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    variant_id = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    file_link = models.TextField()
+
+
 class Answer(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    mark = models.CharField(max_length=10)
-    answer = models.UUIDField(default=uuid.uuid4())
+    complect_id = models.ForeignKey(Complect, on_delete=models.CASCADE)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
+    file_link = models.TextField()
+    mark = models.CharField(max_length=10, null=True)
 
 
 class ThirdMark(models.Model):
@@ -86,3 +107,24 @@ class ThirdMark(models.Model):
 class ThirdMarkExpert(models.Model):
     third_mark = models.ForeignKey(ThirdMark, on_delete=models.CASCADE)
     expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
+
+
+class EventStatus(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class OrganizationEvent(models.Model):
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event_status_id = models.ForeignKey(EventStatus, on_delete=models.CASCADE)
+    percent_status = models.CharField(max_length=3)
+
+
+class ScannedPage(models.Model):
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    complect_id = models.ForeignKey(Complect, on_delete=models.CASCADE, null=True)
+    file_link = models.TextField()
