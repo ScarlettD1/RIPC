50f23d03cd977d5a63d9ccdf67d0a7cdc9ac6039
Ilya Barinov <81916955+ilya422@users.noreply.github.com>
2023-05-01 23:16:20 +0500

    Добавил скрытие элементов на кнопку (Создание мп)

--- ripc/static/js/pages/create_event.js
+++ ripc/static/js/pages/create_event.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+
+});
+
+$('.page-block .head .btn').click(function(){
+    let mainParent = $(this).parent().parent()
+    let mainChildren = mainParent.children().not('.head')
+
+    // Скрытие/показ детей
+    if ($(mainChildren).is(':hidden')) {
+        mainChildren.show()
+        $(this).find('i').attr("class", "fas fa-chevron-down")
+    }
+    else {
+        mainChildren.hide()
+        $(this).find('i').attr("class", "fas fa-chevron-right")
+    }
+
+});
+
 $('#inputCalendar .input-daterange').datepicker({
     format: "dd/mm/yyyy",
     language: "ru",
