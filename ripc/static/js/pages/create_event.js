81c1a82e8b60ca22c914d094a6e6708f056665d5
Ilya Barinov <81916955+ilya422@users.noreply.github.com>
2023-05-02 02:48:16 +0500

    Добавил отправку основных настроек (Создание мп)

--- ripc/static/js/pages/create_event.js
+++ ripc/static/js/pages/create_event.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 });
 
+// Отслеживание нажатий на скрытие/показ
 $('.page-block .head .btn').click(function(){
     let mainParent = $(this).parent().parent()
     let mainChildren = mainParent.children().not('.head')
@@ -18,6 +19,7 @@ $('.page-block .head .btn').click(function(){
 
 });
 
+// Функция для календаря
 $('#inputCalendar .input-daterange').datepicker({
     format: "dd/mm/yyyy",
     language: "ru",
@@ -26,11 +28,10 @@ $('#inputCalendar .input-daterange').datepicker({
     todayHighlight: true
 });
 
+// Функция для файлов
 $("#inputEventFiles").fileinput({
     theme: "fa5",
     language: "ru",
-    uploadUrl: "/",
-    uploadAsync: true,
     allowedFileExtensions: ["pdf"],
     removeFromPreviewOnError: true,
     browseClass: "btn btn-info",
@@ -40,11 +41,48 @@ $("#inputEventFiles").fileinput({
     enableResumableUpload: true,
     initialPreviewAsData: true,
     preferIconicPreview: true,
-    previewFileIcon: '<i class="fas fa-file"></i>',
     previewFileIconSettings: {
         'pdf': '<i class="fas fa-file-pdf text-danger"></i>',
     },
     fileActionSettings: {
-        showZoom: false
+        showZoom: false,
+        indicatorNew: ''
     }
+});
+
+// Отправка главных настроек
+$("#main-settings-form").submit(function (e) {
+    e.preventDefault();
+    let formData = new FormData();
+
+    // Получаем текстовые данные с формы
+    $.each($(this).serializeArray(),function (key, value) {
+        formData.append(value.name, value.value);
+    })
+
+    // Получаем файлы с формы
+    $.each($(this).find('#inputEventFiles')[0].files, function (key, value) {
+        formData.append("files[]", value, value.name);
+	})
+
+    $.ajax({
+        type: "POST",
+        url: "https://webhook.site/728a2b8c-145f-4367-9de6-c647a227ea2d",
+        processData: false,
+        contentType: false,
+        data: formData,
+        success: function (jqXHR) {
+        // Если успешно - отправить на новый шаг
+            console.log("Основные настройки отправлены!");
+            $('.page-block .main-settings .head .btn').click();
+            $('.page-block .main-settings #main-settings-form .btn').remove();
+            $('.page-block .templates-settings').show()
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, jqXHR.responseText);
+            // $('.page-block .main-settings .head .btn').click();
+            // $('.page-block .main-settings #main-settings-form .btn').remove();
+            // $('.page-block .templates-settings').show()
+        }
+    });
 });
